e9a84b060c75f3ecf0f641582b394c0fadddcf59
zoybzo <zoybzo@outlook.com>
2023-09-30 20:04:53 +0800

    Fixed the '>' bug

--- indent.py
+++ indent.py
@@ -3,7 +3,7 @@ import sys
 
 def indent(file_path):   
     """
-    indent the markdown file
+    indent the headers of the markdown file
     """
     if not os.path.exists(file_path):
         # throw the error
@@ -18,9 +18,19 @@ def indent(file_path):
             if line.startswith('#'):
             # change the beginning of the line to '##'
                 line = '#' + line 
+            # if the line begin with '>' and the next line does not begin with '>', add an empty line after the line
+            if line.startswith('>'):
+                # get the index of the line
+                index = lines.index(line)
+                # get the length of lines
+                length = len(lines)
+                # if the next line does not begin with '>'
+                if index + 1 >= length or not lines[index+1].startswith('>'):
+                    # add an empty line after the line
+                    line += '\n'
             # change the line in the file
             f.write(line)
-            print(line)
+            # print(line)
     # print the success message
     print('The file has been indented successfully')
     return 
